462ffaa8f59078c29aaccabb99bddba2595d1965
NoraXela <aron.alexlucian@yahoo.com>
2024-01-10 00:35:55 +0200

    v0.9

--- ProiectBlack-Scholes.py
+++ ProiectBlack-Scholes.py
@@ -14,29 +14,6 @@ def d1(S,K,T,r,sigma):
 def d2(S,K,T,r,sigma):
     return d1(S,K,T,r,sigma)-sigma*sqrt(T)
 
-# the greeks
-def call_delta(S,K,T,r,sigma):
-    return norm.cdf(d1(S,K,T,r,sigma))
-def call_gamma(S,K,T,r,sigma):
-    return norm.pdf(d1(S,K,T,r,sigma))/(S*sigma*sqrt(T))
-def call_vega(S,K,T,r,sigma):
-    return 0.01*(S*norm.pdf(d1(S,K,T,r,sigma))*sqrt(T))
-def call_theta(S,K,T,r,sigma):
-    return 0.01*(-(S*norm.pdf(d1(S,K,T,r,sigma))*sigma)/(2*sqrt(T)) - r*K*exp(-r*T)*norm.cdf(d2(S,K,T,r,sigma)))
-def call_rho(S,K,T,r,sigma):
-    return 0.01*(K*T*exp(-r*T)*norm.cdf(d2(S,K,T,r,sigma)))
-    
-def put_delta(S,K,T,r,sigma):
-    return -norm.cdf(-d1(S,K,T,r,sigma))
-def put_gamma(S,K,T,r,sigma):
-    return norm.pdf(d1(S,K,T,r,sigma))/(S*sigma*sqrt(T))
-def put_vega(S,K,T,r,sigma):
-    return 0.01*(S*norm.pdf(d1(S,K,T,r,sigma))*sqrt(T))
-def put_theta(S,K,T,r,sigma):
-    return 0.01*(-(S*norm.pdf(d1(S,K,T,r,sigma))*sigma)/(2*sqrt(T)) + r*K*exp(-r*T)*norm.cdf(-d2(S,K,T,r,sigma)))
-def put_rho(S,K,T,r,sigma):
-    return 0.01*(-K*T*exp(-r*T)*norm.cdf(-d2(S,K,T,r,sigma)))
-
 # functii call si put
 def bs_call(S,K,T,r,sigma):
     return S*norm.cdf(d1(S,K,T,r,sigma))-K*exp(-r*T)*norm.cdf(d2(S,K,T,r,sigma))
